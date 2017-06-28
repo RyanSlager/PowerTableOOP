020196b59bf0d8a76fec499568e77d895d57373e
danteCLRK <fryanslaucerers@gmail.com>
2017-06-28 15:34:40 -0400

    added GetPowers() to number class which returns a 2d array of powers

--- PowerTable/Program.cs
+++ PowerTable/Program.cs
@@ -10,6 +10,20 @@ namespace PowerTable
     {
         static void Main(string[] args)
         {
+
+            Number num = new Number(50);
+
+            var powers = num.GetPowers();
+            int x = powers.GetLength(0);
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine(powers[i, j]);
+                }
+            }
+
         }
     }
 }
